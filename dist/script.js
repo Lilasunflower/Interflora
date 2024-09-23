d5c3d97ca6eed885d541d7dc4204bcef2dc48331
Lilasunflower <dalilatheartist127@gmail.com>
2024-09-22 22:09:25 -0600

    Revert "Feature/structure refactoring"

--- dist/script.js
+++
@@ -1,7 +0,0 @@
-const $burgerButton = document.querySelector("#js-burger-btn");
-const $navbarTarget = document.querySelector("#js-navbar-target");
-
-$burgerButton.onclick = function() {
-  $burgerButton.classList.toggle("js-active");
-  $navbarTarget.classList.toggle("js-active");
-};
